79a710e72d77dc4852786c7dd195df6625c16ed0
willkang7 <willkang7@gmail.com>
2020-12-01 00:12:34 -0500

    Fixed indentation.

--- pong.py
+++ pong.py
@@ -48,45 +48,45 @@ wn.onkeypress(paddle_b.move_down, "Down")
 
 # Main game loop
 while True:
-    wn.update()
-    
-    # Move the ball
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
+	wn.update()
+	
+	# Move the ball
+	ball.setx(ball.xcor() + ball.dx)
+	ball.sety(ball.ycor() + ball.dy)
 
-    # Border checking
+	# Border checking
 
-    # Top and bottom
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ball.dy *= -1
-        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
-    
-    elif ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
-        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+	# Top and bottom
+	if ball.ycor() > 290:
+		ball.sety(290)
+		ball.dy *= -1
+		winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+	
+	elif ball.ycor() < -290:
+		ball.sety(-290)
+		ball.dy *= -1
+		winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
 
-    # Left and right
-    if ball.xcor() > 350:
-        score_a += 1
-        pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        ball.dx *= -1
+	# Left and right
+	if ball.xcor() > 350:
+		score_a += 1
+		pen.clear()
+		pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+		ball.goto(0, 0)
+		ball.dx *= -1
 
-    elif ball.xcor() < -350:
-        score_b += 1
-        pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
-        ball.goto(0, 0)
-        ball.dx *= -1
+	elif ball.xcor() < -350:
+		score_b += 1
+		pen.clear()
+		pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+		ball.goto(0, 0)
+		ball.dx *= -1
 
-    # Paddle and ball collisions
-    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
-        ball.dx *= -1 
-        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
-    
-    elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
-        ball.dx *= -1
-        winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+	# Paddle and ball collisions
+	if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+		ball.dx *= -1 
+		winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
+	
+	elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+		ball.dx *= -1
+		winsound.PlaySound("sounds/bounce.wav", winsound.SND_ASYNC)
